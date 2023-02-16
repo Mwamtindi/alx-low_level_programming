090a6433aa8b32e8fce6d21a22dc7dad54c26020
Mwamtindi <mwamtindishaban@gmail.com>
2023-02-16 14:12:09 +0000

    Hello,printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 
 /**
- * printf with proper grammar, but the outcome is a piece of art
+ * main - prints with proper grammar, but the outcome is a piece of art
  * Return: Always 0
  */
 int main(void)

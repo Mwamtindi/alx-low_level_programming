d3afd9d2dbde5d406a92b047b78f2a458d45f426
Mwamtindi <mwamtindishaban@gmail.com>
2023-02-17 20:16:50 +0000

    Patience, persistence and perspiration

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -4,38 +4,25 @@
  * main - prints all possible combinations of single-digit numbers
  * Return: Always 0.
  */
-
 int main(void)
 {
-	int c, j, k;
+	int c;
 
-	for (c = 0; c < 10; c++)
+	for (c = '0'; c <= '9'; c++)
 	{
-		for (j = c; j < 10; j++)
+		putchar(c);
+
+		if (c != '9')
 		{
-			for (k = j; k < 10; k++)
-			{
-				putchar(c + '0');
+			putchar(',');
 
-				putchar(',');
-
-				putchar(' ');
-
-				putchar(j + '0');
-
-				putchar(',');
-
-				putchar(' ');
-
-				putchar(k + '0');
-
-				putchar('\n');
-
-			}
+			putchar(' ');
 
 		}
 
 	}
+
+	putchar('\n');
 
 	return (0);
 

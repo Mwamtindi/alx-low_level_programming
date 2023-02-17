7c47038be245df3a3d13f053476348be259ae61b
Mwamtindi <mwamtindishaban@gmail.com>
2023-02-17 13:34:05 +0000

    I sometimes suffer from insomnia

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet in lowercase, followed by a new line.
+ * Return: Always 0.
+ */
+
+int main(void)
+
+{
+	char c = 'a';
+	do
+	{
+		putchar(c);
+		c++;
+	}
+
+	while (c <= 'z');
+	putchar('\n');
+
+	return (0);
+
+}

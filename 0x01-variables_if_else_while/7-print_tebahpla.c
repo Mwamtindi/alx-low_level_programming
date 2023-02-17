75b60e0d44cdc06e34a2c920e6cada78390fea4a
Mwamtindi <mwamtindishaban@gmail.com>
2023-02-17 15:53:32 +0000

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - prints the lowercase alphabet in reverse
+ * Return: Always 0;
+ */
+
+int main(void)
+{
+	char c = 'z';
+	do
+	{
+		putchar(c);
+		c--;
+	}
+
+	while (c >= 'a');
+	putchar('\n');
+
+	return (0);
+
+}

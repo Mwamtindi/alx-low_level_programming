67a30a7a8c2ec1199e69ccda82bf731d098a621c
Mwamtindi <mwamtindishaban@gmail.com>
2023-02-17 18:05:01 +0000

    Hexadecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,11 +9,13 @@ int main(void)
 {
 	int x;
 
-	for (x = 0; x < 10; x++) {
+	for (x = 0; x < 10; x++)
+	{
 		putchar(x + '0');
 	}
 
-	for (x = 0; x < 6; x++) {
+	for (x = 0; x < 6; x++)
+	{
 		putchar(x + 'a');
 	}
 

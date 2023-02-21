960b1c4fcf7ad76633641dd0d0b96ad971858dd4
Mwamtindi <mwamtindishaban@gmail.com>
2023-02-21 17:32:21 +0000

    isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -5,8 +5,7 @@
 /**
  * _isalpha - checks alphabetic character.
  * @c: The character to be checked
- * Return : On success 1.
- * Otherwise 0 is returned.
+ * Return : 1 if c is lowercase or uppercase, 0 otherwise.
  */
 
 int _isalpha(int c)

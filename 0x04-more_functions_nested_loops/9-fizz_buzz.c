6f13ed73822144c25c28c423a2a657d9fa2db965
Mwamtindi <mwamtindishaban@gmail.com>
2023-02-24 06:52:33 +0000

    Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -16,19 +16,23 @@ int main(void)
 	{
 		if (p % 3 == 0 && p % 5 == 0)
 		{
-			printf("FizzBuzz ");
+			printf("FizzBuzz");
 		}
 		else if (p % 3 == 0)
 		{
-			printf("Fizz ");
+			printf("Fizz");
 		}
 		else if (p % 5 == 0)
 		{
-			printf("Buzz ");
+			printf("Buzz");
 		}
 		else
 		{
-			printf("%d ", p);
+			printf("%d", p);
+		}
+		if (p != 100)
+		{
+			printf(" ");
 		}
 	}
 

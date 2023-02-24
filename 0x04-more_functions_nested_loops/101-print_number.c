52a3fb778b07b37463665915e736b0d50e502119
Mwamtindi <mwamtindishaban@gmail.com>
2023-02-24 07:13:51 +0000

    Numbers have life

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -9,16 +9,20 @@
 
 void print_number(int n)
 {
+	unsigned int f = n;
+
 	if (n < 0)
 	{
 		_putchar('-');
 		n = -n;
+		f = n;
 	}
 
-	if (n / 10)
+	f /= 10;
+	if (f != 0)
 	{
-		print_number(n / 10);
+		print_number(f);
 	}
 
-	_putchar(n % 10 + '0');
+	_putchar((unsigned int) n % 10 + '0');
 }

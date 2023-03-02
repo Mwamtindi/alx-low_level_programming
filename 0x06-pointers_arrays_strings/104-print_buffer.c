be1269bad0d79286522e4e4660883343c7c85562
Mwamtindi <mwamtindishaban@gmail.com>
2023-03-02 13:25:15 +0000

    print_buffer

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -10,43 +10,43 @@
 
 void print_buffer(char *b, int size)
 {
-	int aa, bb, lc;
+	int aa, cc;
+
+	for (aa = 0; aa < size; aa += 10)
+	{
+		printf("%08x: ", aa);
+
+		for (cc = 0; cc < 10; cc++)
+		{
+			if ((cc + aa) >= size)
+				printf(" ");
+
+			else
+				printf("%02x", *(b + cc + aa));
+
+			if ((cc % 2) != 0 && cc != 0)
+			{
+				printf(" ");
+			}
+		}
+		for (cc = 0; cc < 10; cc++)
+		{
+			if ((cc + aa) >= size)
+				break;
+
+			else if (*(b + cc + aa) >= 31
+				 && *(b + cc + aa) <= 126)
+				printf("%c", *(b + cc + aa));
+			else
+				printf(".");
+		}
+
+		if (aa >= size)
+			continue;
+
+		printf("\n");
+	}
 
 	if (size <= 0)
-	{
 		printf("\n");
-		return;
-	}
-	lc = (size - 1) / 10 + 1;
-	for (aa = 0; aa < lc; aa++)
-	{
-		printf("%08x: ", aa * 10);
-		for (bb = 0; bb < 10; bb++)
-		{
-			if (aa * 10 + bb < size)
-			{
-				printf("%02x", b[aa * 10 + bb]);
-			}
-			else
-			{
-				printf("  ");
-			}
-			if (bb % 2 == 1)
-			{
-				printf(" ");
-			}
-		}
-		for (bb = 0; bb < 10; bb++)
-		{
-			if (aa * 10 + bb < size)
-			{
-				printf("%c", isprint(b[aa * 10 + bb]) ? b[aa * 10 + bb] : '.');
-			}
-			else
-			{
-				printf(" ");
-			}
-		}
-		printf("\n");
-	}
 }

42de45de56441ff40f9f8a756ced396b0d247692
Mwamtindi <mwamtindishaban@gmail.com>
2023-03-02 09:14:51 +0000

    print_number

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -9,14 +9,17 @@
 
 void print_number(int n)
 {
+	unsigned int d;
+
+	d = n;
 	if (n < 0)
 	{
 		_putchar('-');
-		n = -n;
+		d = -n;
 	}
-	if (n / 10 != 0)
+	if (d / 10 != 0)
 	{
-		print_number(n / 10);
+		print_number(d / 10);
 	}
-	_putchar(n % 10 + '0');
+	_putchar((d % 10) + '0');
 }

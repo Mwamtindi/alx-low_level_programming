19de1b6754f2cb080cc9e23615c95c318ea988d8
Mwamtindi <mwamtindishaban@gmail.com>
2023-03-02 05:32:14 +0000

    cap_string

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+#include <ctype.h>
+
+/**
+ * cap_string - capitalizes all words of a string.
+ * @str: the pointer to be used.
+ * Return: On success str.
+ */
+
+char *cap_string(char *str)
+{
+	int c = 1;
+	char *k = str;
+
+	while (*k != '\0')
+	{
+		if (isspace(*k) || ispunct(*k))
+		{
+			c = 1;
+		}
+		else if (c)
+		{
+			*k = toupper(*k);
+			c = 0;
+		}
+		k++;
+	}
+	return (str);
+}

fbb579a569be7882347fad42c7279c01b2c3ec1f
Mwamtindi <mwamtindishaban@gmail.com>
2023-03-04 10:48:26 +0000

    first 98 Fibonacci

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,6 +1,27 @@
 #include <stdio.h>
 
 /**
+ * fibnum_length - return the length to be used.
+ * @n: the number to be used.
+ * Return: On success len.
+ */
+
+int fibnum_length(int n)
+{
+	int len = 0;
+
+	if (!n)
+	{
+		return (1);
+	}
+	while (n)
+	{
+		n = n / 10;
+		len++;
+	}
+	return (len);
+}
+/**
  * main - check the code and prints the first 98 Fibonacci numbers.
  *
  * Return: Always 0.
@@ -8,24 +29,34 @@
 
 int main(void)
 {
-	int a = 1, b = 2, c, i;
+	unsigned long acc, nf = 100000000, fn1 = 0, fn2 = 0, acci = 0;
+	unsigned long fib1 = 1, fib2 = 2;
+	short int i = 1, fibnum;
 
-	printf("%d, %d, ", a, b);
-
-	for (i = 3; i < 98; i++)
+	while (i <= 98)
 	{
-		c = a + b;
-		printf("%d, ", c);
-
-		if (i < 98)
+		if (fn1 > 0)
+			printf("%lu", fn1);
+		fibnum = fibnum_length(nf) - 1 - fibnum_length(fib1);
+		while (fn1 > 0 && fibnum > 0)
 		{
-			printf(",");
+			printf("%i", 0);
+			fibnum--;
 		}
-		a = b;
-		b = c;
+		printf("%lu", fib1);
+
+		acc = (fib1 + fib2) % nf;
+		acci = fn1 + fn2 + (fib1 + fib2) / nf;
+		fib1 = fib2;
+		fn1 = fn2;
+		fib2 = acc;
+		fn2 = acci;
+
+		if (i != 98)
+			printf(", ");
+		else
+			printf("\n");
+		i++;
 	}
-
-	printf("\n");
-
 	return (0);
 }

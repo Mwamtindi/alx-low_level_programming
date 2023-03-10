1c8ec03ea09a58f5c85e29ed270d9ef1c576a7fa
Mwamtindi <mwamtindishaban@gmail.com>
2023-03-10 18:43:51 +0000

    prints the number

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - check the code
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: Always 0.
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argv;
+
+	printf("%d\n", argc - 1);
+	return (0);
+}

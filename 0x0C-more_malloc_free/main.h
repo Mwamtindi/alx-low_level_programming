563d10bc9ce1d6d32f98766072c7340a6f42191e
Mwamtindi <mwamtindishaban@gmail.com>
2023-03-17 12:39:27 +0000

    main header file

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,3 +1,6 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
 int _putchar(char c);
 
 void *malloc_checked(unsigned int b);
@@ -10,3 +13,4 @@ int *array_range(int min, int max);
 
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 
+#endif

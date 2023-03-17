2dc25d1bc6e9f99710bc3ed8730f33924aed692c
Mwamtindi <mwamtindishaban@gmail.com>
2023-03-17 16:32:17 +0000

    multiplication of two +ve num

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,82 +1,102 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <ctype.h>
 #include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
 
-int _numpositive(char *str);
+#define ERR_MSG "Error"
 
 /**
- * _numpositive - check for correct argument and positive numbers
- * @str: memory address;
- * Return: On success 0 else 1 to be returned.
+ * _isdigit - checks if a string contains a non-digit char
+ * @c: string to be checked
+ *
+ * Return: 0 if a non-digit is found, else 1 is returned.
  */
-
-int _numpositive(char *str)
+int _isdigit(char *c)
 {
-	int e = 0;
+	int u = 0;
 
-	while (str[e] != '\0')
+	while (c[u])
 	{
-		if (!isdigit(str[e]))
-		{
+		if (c[u] < '0' || c[u] > '9')
 			return (0);
-		}
-		e++;
+		u++;
 	}
 	return (1);
 }
+
+/**
+ * _strlen - returns the length of a string
+ * @n: string to be checked
+ *
+ * Return: the size of the string.
+ */
+int _strlen(char *n)
+{
+	int u = 0;
+
+	while (n[u] != '\0')
+	{
+		u++;
+	}
+	return (u);
+}
+
+/**
+ * _iserror - checks for errors
+ * Return: Nothing.
+ */
+void _iserror(void)
+{
+	printf("Error\n");
+	exit(98);
+}
+
 /**
  * main - check the code
  * @argc: argument count
  * @argv: argument vector
- * Return: on success 0.
+ *
+ * Return: On success 0.
  */
-
 int main(int argc, char *argv[])
 {
-	char *mult;
-	int num1, num2, prod, d, k, e = 0;
-	int digits[100];
+	char *sp1, *sp2;
+	int sha1, sha2, sha0, u, raw, num1, num2, *prod, e = 0;
 
-	if (argc != 3)
+	sp1 = argv[1], sp2 = argv[2];
+	if (argc != 3 || !_isdigit(sp1) || !_isdigit(sp2))
+		_iserror();
+	sha1 = _strlen(sp1);
+	sha2 = _strlen(sp2);
+	sha0 = sha1 + sha2 + 1;
+	prod = malloc(sizeof(int) * sha0);
+	if (!prod)
+		return (1);
+	for (u = 0; u <= sha1 + sha2; u++)
+		prod[u] = 0;
+	for (sha1 = sha1 - 1; sha1 >= 0; sha1--)
 	{
-		mult = "Error\n";
-		while (*mult)
+		num1 = sp1[sha1] - '0';
+		raw = 0;
+		for (sha2 = _strlen(sp2) - 1; sha2 >= 0; sha2--)
 		{
-			_putchar(*mult++);
+			num2 = sp2[sha2] - '0';
+			raw += prod[sha1 + sha2 + 1] + (num1 * num2);
+			prod[sha1 + sha2 + 1] = raw % 10;
+			raw /= 10;
 		}
-		exit(98);
+		if (raw > 0)
+			prod[sha1 + sha2 + 1] += raw;
 	}
-	if (!_numpositive(argv[1]) || !_numpositive(argv[2]))
+	for (u = 0; u < sha0 - 1; u++)
 	{
-		mult = "Error\n";
-		while (*mult)
-		{
-			_putchar(*mult++);
-		}
-		exit(98);
+		if (prod[u])
+			e = 1;
+		if (e)
+			_putchar(prod[u] + '0');
 	}
-	num1 = atoi(argv[1]);
-	num2 = atoi(argv[2]);
-
-	prod = num1 * num2;
-	if (prod == 0)
-	{
+	if (!e)
 		_putchar('0');
-		_putchar('\n');
-		return (0);
-	}
-	while (prod > 0)
-	{
-		d = prod % 10;
-		prod /= 10;
-		digits[e++] = d;
-	}
-	for (k = e - 1; k >= 0; k--)
-	{
-		_putchar(digits[k] + '0');
-	}
 	_putchar('\n');
-
+	free(prod);
 	return (0);
 }

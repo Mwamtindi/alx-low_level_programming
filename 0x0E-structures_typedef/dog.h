0fd6cd3e5a9c56641e807980ce7dc4115c8ee0fd
Mwamtindi <mwamtindishaban@gmail.com>
2023-03-20 20:23:36 +0000

    Poppy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,15 @@
 #ifndef HEADER_FILE_NAME_H
 #define HEADER_FILE_NAME_H
 
-struct dog {
+/**
+ * struct dog - info of the dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ */
+
+struct dog
+{
 	char *name;
 	float age;
 	char *owner;

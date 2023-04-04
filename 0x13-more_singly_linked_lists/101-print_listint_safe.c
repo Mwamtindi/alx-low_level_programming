32f7820e91e4bd7c443d00d1d13ff80428ee65c8
Mwamtindi <mwamtindishaban@gmail.com>
2023-04-04 18:51:25 +0000

    print(safe version)

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -3,6 +3,53 @@
 #include <string.h>
 #include "lists.h"
 
+size_t _listint_loop(const listint_t *head);
+
+/**
+ * _listint_loop - gives number of  nodes for the looped in the list
+ * @head: A pointer to the head of the listint_t list
+ * Return: 0 If the list is not looped Otherwise,
+ *          the number of nodes in the list id returned.
+ */
+size_t _listint_loop(const listint_t *head)
+{
+	const listint_t *beko, *sung;
+	size_t nod = 1;
+
+	if (head == NULL || head->next == NULL)
+		return (0);
+
+	beko = head->next;
+	sung = (head->next)->next;
+
+	while (sung)
+	{
+		if (beko == sung)
+		{
+			beko = head;
+			while (beko != sung)
+			{
+				nod++;
+				beko = beko->next;
+				sung = sung->next;
+			}
+
+			beko = beko->next;
+			while (beko != sung)
+			{
+				nod++;
+				beko = beko->next;
+			}
+
+			return (nod);
+		}
+
+		beko = beko->next;
+		sung = (sung->next)->next;
+	}
+
+	return (0);
+}
 /**
  * print_listint_safe - fxn that prints a listint_t linked list
  * @head: pointer to the head of listint_t linked list
@@ -11,33 +58,29 @@
 
 size_t print_listint_safe(const listint_t *head)
 {
-	const listint_t *beko, *sung;
+	size_t nod, inx = 0;
 
-	beko = head;
-	sung = head;
+	nod = _listint_loop(head);
 
-	if (head == NULL)
-		exit(98);
-
-	while (beko && sung && sung->next)
+	if (nod == 0)
 	{
-		beko = beko->next;
-		sung = sung->next->next;
-		if (beko == sung)
+		for (; head != NULL; nod++)
 		{
-			printf("-> [%p] %d\n", (void *)beko, beko->n);
-			return (0);
+			printf("[%p] %d\n", (void *)head, head->n);
+			head = head->next;
 		}
-		printf("[%p] %d\n", (void *)beko, beko->n);
 	}
-	printf("[%p] %d\n", (void *)beko, beko->n);
-	beko = beko->next;
-	while (beko && sung && sung->next)
+
+	else
 	{
-		printf("[%p] %d\n", (void *)beko, beko->n);
-		beko = beko->next;
-		sung = sung->next->next;
+		for (inx = 0; inx < nod; inx++)
+		{
+			printf("[%p] %d\n", (void *)head, head->n);
+			head = head->next;
+		}
+
+		printf("-> [%p] %d\n", (void *)head, head->n);
 	}
-	printf("[%p] %d\n", (void *)beko, beko->n);
-	return (0);
+
+	return (nod);
 }

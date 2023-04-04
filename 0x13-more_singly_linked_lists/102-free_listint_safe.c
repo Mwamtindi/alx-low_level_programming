dca4beb59253a1c9c6a63302f4479d31a3670953
Mwamtindi <mwamtindishaban@gmail.com>
2023-04-04 19:05:53 +0000

    free(safe version)

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -3,6 +3,53 @@
 #include <string.h>
 #include "lists.h"
 
+size_t listint_loop(const listint_t *head);
+
+/**
+ * listint_loop - gives number of  nodes for the looped in the list
+ * @head: pointer to the head of the listint_t list
+ * Return: 0 If the list is not looped Otherwise,
+ *          the number of nodes in the list id returned.
+ */
+size_t listint_loop(const listint_t *head)
+{
+	const listint_t *beko, *sung;
+	size_t nod = 1;
+
+	if (head == NULL || head->next == NULL)
+		return (0);
+
+	beko = head->next;
+	sung = (head->next)->next;
+
+	while (sung)
+	{
+		if (beko == sung)
+		{
+			beko = head;
+			while (beko != sung)
+			{
+				nod++;
+				beko = beko->next;
+				sung = sung->next;
+			}
+
+			beko = beko->next;
+			while (beko != sung)
+			{
+				nod++;
+				beko = beko->next;
+			}
+
+			return (nod);
+		}
+
+		beko = beko->next;
+		sung = (sung->next)->next;
+	}
+
+	return (0);
+}
 /**
  * free_listint_safe - fxn that frees a listint_t list
  * @h:pointer to the listint_t list
@@ -11,25 +58,34 @@
 
 size_t free_listint_safe(listint_t **h)
 {
-	size_t tally = 0;
-	listint_t *curr, *pet;
+	listint_t *pet;
+	size_t nod, inx;
 
-	if (h == NULL)
-		return (0);
+	nod = listint_loop(*h);
 
-	curr = *h;
-	while (curr != NULL)
+	if (nod == 0)
 	{
-		tally++;
-		if (curr <= curr->next)
+		for (; h != NULL && *h != NULL; nod++)
 		{
-			*h = NULL;
-			exit(98);
+			pet = (*h)->next;
+			free(*h);
+			*h = pet;
 		}
-		pet = curr;
-		curr = curr->next;
-		free(pet);
 	}
-	*h = NULL;
-	return (tally);
+
+	else
+	{
+		for (inx = 0; inx < nod; inx++)
+		{
+			pet = (*h)->next;
+			free(*h);
+			*h = pet;
+		}
+
+		*h = NULL;
+	}
+
+	h = NULL;
+
+	return (nod);
 }

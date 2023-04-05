3279007d9c97ea1841f14b4b6acd6c781ff30170
Mwamtindi <mwamtindishaban@gmail.com>
2023-04-05 18:12:28 +0000

    assemly code

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,9 +1,11 @@
-global    main
-          extern    printf
-main:
-	  mov   edi, output_format
-	  xor   eax, eax
-	  call  printf
-	  mov 	eax, 0
-	  ret
-output_format: db `Hello, Holberton\n`,0
+global    main    ; declare main fxn as global
+          extern    printf   ; declare printf fxn
+
+main:      ; entry point
+	  mov   edi, output_format   ; move address of the output
+	  xor   eax, eax             ; set the value to 0
+	  call  printf               ; call printf fxn
+	  mov 	eax, 0               ; set the return value
+	  ret                        ; return from main fxn
+
+output_format: db `Hello, Holberton\n`,0  ; define output

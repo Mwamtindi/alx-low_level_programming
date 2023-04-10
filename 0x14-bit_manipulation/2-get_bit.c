cc38d2293a7f493ca60242cd9e48911bd3c66258
Mwamtindi <mwamtindishaban@gmail.com>
2023-04-10 06:47:01 +0000

    value of a bit at a given index

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -13,15 +13,11 @@ int get_bit(unsigned long int n, unsigned int index)
 {
 	unsigned int vibe;
 
+	if (index > 63)
+		return (-1); /* this indicate an error */
+
 	/* right shift n by index to bring bit and assigned it to vibe */
 	vibe = (n >> index) & 1;
 
-	if (vibe == 0 || vibe == 1)
-	{
-		return (vibe);
-	}
-	else
-	{
-		return (-1); /* this indicate an error */
-	}
+	return (vibe);
 }

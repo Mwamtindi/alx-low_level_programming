58048f3c4c86159f96ae75ddb173144b87171716
Mwamtindi <mwamtindishaban@gmail.com>
2023-04-10 05:13:22 +0000

    creates a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -24,7 +24,7 @@ int create_file(const char *filename, char *text_content)
 		for (siz = 0; text_content[siz]; siz++)
 			;
 	}
-	ap = open(filename, O_CREATE | O_RDWR | O_TRUNC, 0600); /* open file */
+	ap = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600); /* open file */
 	tf = write(ap, text_content, siz); /* write the string */
 
 	if (ap == -1 || tf == -1)

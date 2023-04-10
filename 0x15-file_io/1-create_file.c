bd2920047b4dca70b6c221ff76b438d08111bbbd
Mwamtindi <mwamtindishaban@gmail.com>
2023-04-10 04:57:37 +0000

    creates a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -6,6 +6,8 @@
  * create_file - a fxn that creates a file
  * @filename: name of the file to be created
  * @text_content: a NULL terminated string to write to the file (filename)
+ * file must have those permissions: rw-------. If the file alrwady exists,
+ * do not change the permissions
  * Return: 1 on success creation
  *        -1 if fxn fails ie file can not be created, can't be written,
  *        "write" fails, etc... or filename is NULL.
@@ -23,12 +25,8 @@ int create_file(const char *filename, char *text_content)
 		while (text_content[siz])
 			siz++;
 	}
-	/**
-	 * open file with flags and store the file desc in ap
-	 * write the string to the file and store num of bytes
-	 */
-	ap = open(filename, O_CREATE | O_RDWR | O_TRUNC, 0600);
-	tf = write(ap, text_content, siz);
+	ap = open(filename, O_CREATE | O_RDWR | O_TRUNC, 0600); /* open file */
+	tf = write(ap, text_content, siz); /* write the string */
 
 	if (ap == -1 || tf == -1)
 		return (-1);

0cce8e545cc70f7bc267d706dab3639722052f34
Mwamtindi <mwamtindishaban@gmail.com>
2023-07-07 16:11:48 -0400

    adds an element to the hash table

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,73 +1,73 @@
 #include "hash_tables.h"
 
 /**
- * make_hash_node - fxn that creates a new hash node
- * @key: the key for the node
- * @value: the value for the node
+ * make_hash_node - creates a new hash node
+ * @key: key for the node
+ * @value: for the node
  *
- * Return: the new node, or NULL if fails
+ * Return: the new node, or NULL on failure
  */
 hash_node_t *make_hash_node(const char *key, const char *value)
 {
-	hash_node_t *_node;
+	hash_node_t *node;
 
-	_node = malloc(sizeof(hash_node_t));
-	if (_node == NULL)
+	node = malloc(sizeof(hash_node_t));
+	if (node == NULL)
 		return (NULL);
-	_node->key = strdup(key);
-	if (_node->key == NULL)
+	node->key = strdup(key);
+	if (node->key == NULL)
 	{
-		free(_node);
+		free(node);
 		return (NULL);
 	}
-	_node->value = strdup(value);
-	if (_node->value == NULL)
+	node->value = strdup(value);
+	if (node->value == NULL)
 	{
-		free(_node->key);
-		free(_node);
+		free(node->key);
+		free(node);
 		return (NULL);
 	}
-	_node->next = NULL;
-	return (_node);
+	node->next = NULL;
+	return (node);
 }
 
 
 /**
- * hash_table_set - fxn that sets a key to a value in the hash table
- * @ht: hash table to add element to it
+ * hash_table_set - sets a key to a value in the hash table
+ * @ht: hash table to add elemt to
  * @key: key for the data
  * @value: data to store
  *
- * Return: 1 if succeeded, 0 otherwise
+ * Return: 1 if successful, 0 otherwise
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	unsigned long int indx;
-	hash_node_t *hash_node, *temp;
-	char *_new_value;
+	unsigned long int index;
+	hash_node_t *hash_node, *tmp;
+	char *new_value;
 
 	if (ht == NULL || ht->array == NULL || ht->size == 0 ||
 	    key == NULL || strlen(key) == 0 || value == NULL)
 		return (0);
-	indx = key_indx((const unsigned char *)key, ht->size);
-	temp = ht->array[indx];
-	while (temp != NULL)
+	index = key_index((const unsigned char *)key, ht->size);
+	tmp = ht->array[index];
+	while (tmp != NULL)
 	{
-		if (strcmp(temp->key, key) == 0)
+		if (strcmp(tmp->key, key) == 0)
 		{
-			_new_value = strdup(value);
-			if (_new_value == NULL)
+			new_value = strdup(value);
+			if (new_value == NULL)
 				return (0);
-			free(temp->value);
-			temp->value = _new_value;
+			free(tmp->value);
+			tmp->value = new_value;
 			return (1);
 		}
-		temp = temp->next;
+		tmp = tmp->next;
 	}
 	hash_node = make_hash_node(key, value);
 	if (hash_node == NULL)
 		return (0);
-	hash_node->next = ht->array[indx];
-	ht->array[indx] = hash_node;
+	hash_node->next = ht->array[index];
+	ht->array[index] = hash_node;
 	return (1);
 }

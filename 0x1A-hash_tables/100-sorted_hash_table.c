3599bb504a0a1da991dc6bde4b0e09032968629b
Mwamtindi <mwamtindishaban@gmail.com>
2024-04-14 21:19:11 +0300

    sorted hash table

--- 0x1A-hash_tables/100-sorted_hash_table.c
+++ 0x1A-hash_tables/100-sorted_hash_table.c
@@ -1,4 +1,5 @@
 #include "hash_tables.h"
+#include "sorted_tables.h"
 
 
 /**
@@ -171,27 +172,4 @@ void shash_table_print_rev(const shash_table_t *ht)
 			printf(", ");
 	}
 	printf("}\n");
-}
-
-/**
- * shash_table_delete - fxn that deletes a sorted hash table
- * @ht: sorted hash table to delete
- */
-void shash_table_delete(shash_table_t *ht)
-{
-	shash_node_t *tmp, *tmp2;
-
-	if (ht == NULL)
-		return;
-	tmp = ht->shead;
-	while (tmp)
-	{
-		tmp2 = tmp;
-		tmp = tmp->snext;
-		free(tmp2->key);
-		free(tmp2->value);
-		free(tmp2);
-	}
-	free(ht->array);
-	free(ht);
 }

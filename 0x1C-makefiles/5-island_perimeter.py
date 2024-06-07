7152a430cd99091ca8afea09de2cef68ca26c80f
Mwamtindi <mwamtindishaban@gmail.com>
2024-06-07 08:02:58 +0300

    makefiles

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Module that calculates the perimeter of an island given in a grid."""
 
+
 def per_num_neighbors(grid, k, l):
     """fxt that returns number of neighbors a cell has in a grid."""
 
@@ -16,6 +17,7 @@ def per_num_neighbors(grid, k, l):
         num += 1
 
     return num
+
 
 def island_perimeter(grid):
     """fxn to return perimeter of the island in grid."""

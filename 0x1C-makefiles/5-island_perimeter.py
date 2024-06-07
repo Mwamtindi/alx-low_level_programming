ff0e645d80e7691231a0fad20f9d2aba395c3394
Mwamtindi <mwamtindishaban@gmail.com>
2024-06-07 08:05:58 +0300

    makefiles

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -2,18 +2,18 @@
 """Module that calculates the perimeter of an island given in a grid."""
 
 
-def per_num_neighbors(grid, k, l):
+def per_num_neighbors(grid, k, q):
     """fxt that returns number of neighbors a cell has in a grid."""
 
     num = 0
 
-    if k <= 0 or not grid[k - 1][l]:
+    if k <= 0 or not grid[k - 1][q]:
         num += 1
-    if l <= 0 or not grid[k][l - 1]:
+    if q <= 0 or not grid[k][q - 1]:
         num += 1
-    if l >= len(grid[k]) - 1 or not grid[k][l + 1]:
+    if q >= len(grid[k]) - 1 or not grid[k][q + 1]:
         num += 1
-    if k >= len(grid) - 1 or not grid[k + 1][l]:
+    if k >= len(grid) - 1 or not grid[k + 1][q]:
         num += 1
 
     return num
@@ -24,8 +24,8 @@ def island_perimeter(grid):
 
     per = 0
     for k in range(len(grid)):
-        for l in range(len(grid[k])):
-            if grid[k][l]:
-                per += per_num_neighbors(grid, k, l)
+        for q in range(len(grid[k])):
+            if grid[k][q]:
+                per += per_num_neighbors(grid, k, q)
 
     return per
